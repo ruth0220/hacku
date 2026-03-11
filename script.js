04cb484b419a7ef29972139f4302c611376075e4
ruth0220 <miumiu626.m@ymobile.ne.jp>
2026-03-12 01:13:14 +0900

    見開きビューア更新

--- script.js
+++ script.js
@@ -1,38 +1,60 @@
-const pages = [
-  "これは1ページ目の文章です。",
-  "これは2ページ目の文章です。",
-  "これは3ページ目の文章です。"
-];
+const pages=["これは1ページ目です。","これは2ページ目です。",
+             "これは3ページ目です。","これは4ページ目です。",
+             "これは5ページ目です。","これは6ページ目です。"];
 
 let currentPage = 0;
 
-const pageText = document.getElementById("page-text");
-const pageIndicator = document.getElementById("page-indicator");
+//ページカウント
+const PageCountLeft = document.getElementById("page-count-left");
+const PageCountRight = document.getElementById("page-count-right");
+
+//ボタン
 const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
 
-function renderPage() {
-  pageText.textContent = pages[currentPage];
-  pageIndicator.textContent = `${currentPage + 1} / ${pages.length}`;
-}
+const pageRight = document.getElementById("page-right");
+const pageLeft = document.getElementById("page-left");
 
-
-
-function nextPage() {
-  if (currentPage < pages.length - 1) {
-    currentPage++;
-    renderPage();
+//次へボタンが押されたらページカウントを増やしupdatePageを呼ぶ
+function nextPage(){
+  if(currentPage < pages.length - 2){
+    currentPage += 2;
+    updatePage();
   }
 }
 
-function prevPage() {
-  if (currentPage > 0) {
-    currentPage--;
-    renderPage();
+//前へボタンが押されたらページカウントを減らす
+function prevPage(){
+  if(currentPage >= 2){
+    currentPage -= 2;
+    updatePage();
   }
 }
 
-prevBtn.addEventListener("click", prevPage);
-nextBtn.addEventListener("click", nextPage);
+//文章を書き換える
+function updatePage(){
+  //右
+  pageRight.textContent = pages[currentPage];
+  PageCountRight.textContent = currentPage + 1;
+  //左
+  if(currentPage + 1 < pages.length){
+    pageLeft.textContent = pages[currentPage + 1]; 
+    PageCountLeft.textContent = currentPage + 2;
+  }else{
+    pageLeft.textContent = "";
+    PageCountRight.textContent = "";
+  }
+}
 
-renderPage();
+//ボタンを押したらページカウントを増減させる関数
+nextBtn.addEventListener("click",nextPage);
+prevBtn.addEventListener("click",prevPage);
+
+function keyboard(e){
+  if(e.key == "ArrowRight")nextPage();
+  if(e.key == "ArrowLeft")prevPage();
+}
+
+document.addEventListener("keydown",keyboard);
+
+updatePage();
